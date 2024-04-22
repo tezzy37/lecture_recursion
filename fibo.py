b7bc9be49624e341fea9c32c5cbbc981e0e92801
Tereza Lusková <tezzy37@seznam.cz>
2024-04-22 09:11:19 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,7 +1,16 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return n
+    elif n == 1:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
-    pass
-
+    n = 6
+    vysledek = recursive_nth_fibo(n)
+    print(vysledek)
 
 if __name__ == "__main__":
     main()
